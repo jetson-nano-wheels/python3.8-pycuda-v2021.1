4131863b2579a9d5a957c6bace7a7b7618d02e47
austinjp <austin.plunkett@gmail.com>
2021-09-05 17:41:20 +0100

    Working wheel (hopefully).
    Previous wheel built, but did not work after importing

--- build.sh
+++ build.sh
@@ -2,11 +2,10 @@
 
 ./init.sh
 source venv/bin/activate
-
 source jetson-nano-wheels/commons/envs.sh
 
-PIP_CONSTRAINT=constraints.txt \
-  pip wheel -vv -c constraints.txt --no-deps \
-  --no-binary pycuda \
-  -w dist \
-  pycuda
+pushd pycuda-2021.1
+mv dist dist-$(date +'%Y-%m-%d-%H:%M:%S') 2>/dev/null
+# pip wheel -vvv --no-use-pep517 --only-binary 'pyopencl' -w dist .
+pip wheel -vvv -w dist .
+popd
--- init.sh
+++ init.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# PyCUDA uses pyopencl which in turn requires this:
+sudo apt-get install ocl-icd-opencl-dev
+
+sudo apt-get install libboost-all-dev libboost-python-dev
+
 if [[ ! -d venv ]] ; then
     python3.8 -m venv venv
     source venv/bin/activate
@@ -14,5 +19,26 @@ fi
 
 source jetson-nano-wheels/commons/envs.sh
 
-# Allow pycuda to install its own dependencies
-# since they are all widely available as wheels.
+pip install mako numpy pybind11
+
+# Note, ensure this uses the Numpy installed in the previous steps.
+pip install --verbose pyopencl==2021.2.6
+
+# Don't fetch the zip file for pycuda, instead fetch the git repo
+# specifying the appropriate branch/tag. When followed by updating the
+# submodules, this ensures all the necessary files are fetched, i.e.
+# those in bpl-subset and pycuda/compyte.
+mv pycuda-2021.1 pycuda-2021.1-$(date +'%Y-%m-%d-%H:%M:%S') 2>/dev/null
+git clone --depth 1 --branch v2021.1 https://github.com/inducer/pycuda pycuda-2021.1
+
+pushd pycuda-2021.1
+
+git submodule update --init --recursive
+
+python configure.py
+
+# python configure.py --no-use-shipped-boost
+# This shouldn't be necessary, but seems to be.
+# sed -i'.bak' 's/"USE_SHIPPED_BOOST", True/"USE_SHIPPED_BOOST", False/' setup.py
+
+popd
